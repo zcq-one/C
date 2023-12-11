aaed15546423031278369fdbea9a6c127696ddc1
zcq-one <ZCQzcq260317@outlook.com>
2023-12-11 21:39:52 +0800

    1.c

--- ModernApproach/program/3/1.c
+++ ModernApproach/program/3/1.c
@@ -3,3 +3,15 @@
         Enter a date (mm/dd/yyyy): 2/17/2011
         You entered the date 20110217
 */
+#include <stdio.h>
+int main(void)
+{
+    int year, month, day;
+
+    printf("Enter a date (mm/dd/yyyy): ");
+    scanf(" %d/ %d/ %d", &month, &day, &year);
+
+    printf("You entered the date %4d%.2d%2d", year, month, day);
+
+    return 0;
+}
