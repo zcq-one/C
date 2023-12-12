b945416747b4ddecad89b3e67ab88b3e1dee9cf7
zcq-one <ZCQzcq260317@outlook.com>
2023-12-12 16:26:54 +0800

    2.c

--- ModernApproach/program/3/2.c
+++ ModernApproach/program/3/2.c
@@ -1,5 +1,6 @@
 /*
     2. 编写一个程序，对用户输入的产品信息进行格式化。程序会话应类似下面这样：
+    
         Enter item number: 583
         Enter unit price: 13.5
         Enter purchase date (mm/dd/yyyy): 10/24/2010
@@ -7,7 +8,7 @@
         Item            Unit            Purchase
                         Price           Date 
         583             $13.50          10/24/2010 
-        
+
     其中，产品编号和日期项采用左对齐方式，单位价格采用右对齐方式，允许美元金额最大取值为9999.99。提示：各个列使用制表符控制。
 */
 
