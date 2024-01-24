3035cd15defb8f1aaf1988d7c57bc18d13c77507
zcq-one <ZCQzcq260317@outlook.com>
2024-01-24 22:23:22 +0800

    /

--- C Prime Plus/ProgrammingExercises/11/03.c
+++ C Prime Plus/ProgrammingExercises/11/03.c
@@ -5,23 +5,41 @@
 
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
+#define LIMIT 81
 
-void get_word(char * st);
+char * get_a_word(char st[]);
 
-int main(void)
-{
-    const int LIMIT = 81;
+int main(void) {
     char word[LIMIT];
+    char * get;
 
-    printf("Enter maximum of %d cahraters:\n", LIMIT - 1);
-    get_word(word);
-    printf("The first word you put is");
-    puts(word);
+    printf("Enter maximum of %d characters:\n", LIMIT - 1);
+    get = get_a_word(word);
 
-    return 0;
+    printf("The first word you put is: ");
+    puts(get == NULL ? "No word" : word);
 }
 
-void get_word(char * st)
+char * get_a_word(char st[])
 {
-    
+    int i, j;
+    char temp[LIMIT];
+
+    fgets(temp, LIMIT, stdin);
+
+    for (i = 0; i < strlen(temp) && isalpha(temp[i]) == 0; i++)
+    {
+        if (i == strlen(temp) - 1 && isalpha(temp[i]) == 0)
+            return NULL;
+        else
+            continue;
+    }
+
+    for (j = 0; i < strlen(temp) && isalpha(temp[i]) != 0; i++, j++)
+        st[j] = temp[i];
+
+    st[j] = '\0';
+
+    return st;
 }
