291d6eb58804f36a08ed8b6bc18ee056c693203b
zcq-one <ZCQzcq260317@outlook.com>
2024-01-26 20:15:24 +0800

    /

--- C Prime Plus/ProgrammingExercises/11/06.c
+++ C Prime Plus/ProgrammingExercises/11/06.c
@@ -3,24 +3,3 @@
  * 如果指定字符在字符串中，该函数返回一个非零值（即为真）。否则，返回0（即为假）。
  * 在一个完整的程序中测试该函数，使用一个循环给函数提供输入值。
 */
-
-#include <stdio.h>
-#include <string.h>
-#define SIZE 81
-
-int is_within(int c, char * st);
-
-int main(void)
-{
-    char st[SIZE];
-    char c;
-
-    
-}
-
-int is_within(int c, char * st)
-{
-    
-
-    return 0;
-}
